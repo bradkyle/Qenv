e0a026b529511e3136c745fa3065a72228d77d63
Brad Duncan <bradkyleduncan@gmail.com>
2020-07-04 22:43:32 +0200

    Creating multiagent class

--- qenv/multiagent.py
+++ qenv/multiagent.py
@@ -1,12 +1,26 @@
-
+from gym import Env, spaces
+from qpython import qconnection
 
 class MultiAgentEnv():
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    def _exec(self, qry):
+        with qconnection.QConnection(
+            host=self.host, 
+            port=self.port, 
+            pandas=False) as q:
+            return q.sendSync(qry) 
+
+    def _set_action_space(self):
+        pass
+
+    def _set_observation_space(self):
+        pass
 
     def step(self):
         pass
 
     def reset(self):
         pass
-        
