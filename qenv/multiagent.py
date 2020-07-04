8ca2df08ff2ebf76f7bcf42f9fd2f79807b14663
Brad Duncan <bradkyleduncan@gmail.com>
2020-07-04 22:38:57 +0200

    Creating multiagent class

--- qenv/multiagent.py
+++ qenv/multiagent.py
@@ -1,3 +1,12 @@
 
 
 class MultiAgentEnv():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def step(self):
+        pass
+
+    def reset(self):
+        pass
+        
