a461534d327f27593ec9ddeeb3f81831af47ac62
Brad Duncan <bradkyleduncan@gmail.com>
2020-08-27 01:48:12 +0200

    Liquidation, account margin, initial margin etc.

---
+++ qenv/config.py
@@ -0,0 +1,16 @@
+
+class ConfigDatum():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class Account():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class Inventory():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class Instrument():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
