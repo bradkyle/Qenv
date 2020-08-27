e7f46b7e893212a18f4d1f27db776f71cfd9df1e
Brad Duncan <bradkyleduncan@gmail.com>
2020-08-27 03:10:28 +0200

    Liquidation, account margin, initial margin etc.

--- qenv/config.py
+++ qenv/config.py
@@ -26,7 +26,7 @@ class Env():
 
 [
     {
-        "name":"bitmex_1",
+        "name":"bitmex_mm_1",
         "config":{
             "env":{
                 "adapter":{
@@ -118,5 +118,99 @@ class Env():
                 }
             ]
         }
-    }
+    },
+    {
+        "name":"bitmex_mm_1",
+        "config":{
+            "env":{
+                "adapter":{
+                    "kind":"marketmaker"
+                },
+                "step":{
+                    "kind":"temporal",
+                    "interval":5 #Seconds
+                },
+                "minibatch":{
+                    "kind":"chronological",
+                    "size":86400 #Seconds
+                },
+                "balance":{
+                    "min":0,
+                    "max":100,
+                },
+                "observation":{ # TODO 
+                    "window":100,
+                },
+                "reward":{
+                    "window":300,
+                }
+            },
+            "engine":{
+                # TODO
+            },
+            "instrument":{
+                "contractType":"",
+                "quoteAsset":"",
+                "baseAsset":"",
+                "underlyingAsset": "",
+                "faceValue":"",
+                "maxLeverage":"",
+                "minLeverage":"",
+                "tickSize":"",
+                "priceMultiplier":"",
+                "sizeMultiplier":"",
+                "maxPrice":"",
+                "minPrice":"",
+                "lotSize":"",
+                "junkOrderSize":"",
+                "liquidationStrat":{},
+                "insuranceFee":"",
+                "maxOpenOrders":0,
+                "feeTiers":{
+                    "kind":"flat",
+                    "vol": 0,
+                    "makerFee":-0.00025,
+                    "takerFee":0.00075,
+                    "wdrawFee":0,
+                    "dpsitFee":0,
+                    "wdrawLimit":0
+                },
+                "riskTiers":{
+                    "kind":"procedural",
+                    "baserl":200,
+                    "step":100,
+                    "maintM":0.0035,
+                    "initM":0.01,
+                    "maxLev":100,
+                    "numTier":40
+                }
+            },
+            "accounts":[
+                {
+                    "account":{"balance":0,"marginType":"cross","positionType":"combined","leverage":100},
+                    "shortInventory":{"amt":0,"leverage":0},
+                    "longInventory":{"amt":0,"leverage":0},
+                    "bothInventory":{"amt":0,"leverage":0},
+                },
+                {
+                    "account":{"balance":0,"marginType":"cross","positionType":"combined","leverage":100},
+                    "shortInventory":{"amt":0,"leverage":0},
+                    "longInventory":{"amt":0,"leverage":0},
+                    "bothInventory":{"amt":0,"leverage":0},
+                },
+                {
+                    "account":{"balance":0,"marginType":"cross","positionType":"combined","leverage":100},
+                    "shortInventory":{"amt":0,"leverage":0},
+                    "longInventory":{"amt":0,"leverage":0},
+                    "bothInventory":{"amt":0,"leverage":0},
+                },
+                {
+                    "account":{"balance":0,"marginType":"cross","positionType":"combined","leverage":100},
+                    "shortInventory":{"amt":0,"leverage":0},
+                    "longInventory":{"amt":0,"leverage":0},
+                    "bothInventory":{"amt":0,"leverage":0},
+                }
+            ]
+        }
+    },
 ]
