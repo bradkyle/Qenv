bd2367dca90d41045d529dc9b46af361b960dfd5
Brad Duncan <bradkyleduncan@gmail.com>
2020-09-12 01:01:25 +0200

    Adding validation to config

--- qenv/env/config/validate.py
+++ qenv/env/config/validate.py
@@ -1,8 +1,21 @@
+# https://pypi.org/project/python-json-config/
+# https://pypi.org/project/json-cfg/
+
 from python_json_config import ConfigBuilder
 import json
 
-def Validate(conf):
+def Validatev1(conf):
     builder = ConfigBuilder()
     
+    # Add all validation constructs here
+
+    config = builder.parse_config(conf)
+    return config.to_json()
+
+def Validatev2(conf):
+    builder = ConfigBuilder()
     
-    return builder.parse_config(conf)
+    # Add all validation constructs here
+
+    config = builder.parse_config(conf)
+    return config.to_json()
