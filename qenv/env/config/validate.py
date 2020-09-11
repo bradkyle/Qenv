e77e6297c714292cfa34bb0a789b497a646c54a0
Brad Duncan <bradkyleduncan@gmail.com>
2020-09-12 01:09:04 +0200

    Adding validation to config

--- qenv/env/config/validate.py
+++ qenv/env/config/validate.py
@@ -1,8 +1,9 @@
 
 import json
 
+# uses python-json-config
+# https://pypi.org/project/python-json-config/
 def Validatev1(conf):
-    # https://pypi.org/project/python-json-config/
     from python_json_config import ConfigBuilder
     builder = ConfigBuilder()
     
@@ -11,13 +12,39 @@ def Validatev1(conf):
     config = builder.parse_config(conf)
     return config.to_json()
 
+# uses json-cfg
+# https://pypi.org/project/json-cfg/
 def Validatev2(conf):
-    # https://pypi.org/project/json-cfg/
     import datetime
     import jsoncfg
     from jsoncfg import JSONValueMapper
     from jsoncfg.value_mappers import require_integer
-    
+
+    class OneOf(JSONValueMapper):
+        def __init__(self, *enum_members):
+            self.enum_members = set(enum_members)
+
+        def __call__(self, v):
+            if v not in self.enum_members:
+                raise ValueError('%r is not one of these: %r' % (v, self.enum_members))
+            return v
+
+    class RangeCheck(JSONValueMapper):
+        def __init__(self, min_, max_):
+            self.min = min_
+            self.max = max_
+
+        def __call__(self, v):
+            if self.min <= v < self.max:
+                return v
+            raise ValueError('%r is not in range [%r, %r)' % (v, self.min, self.max))
+
+    class ToDateTime(JSONValueMapper):
+        def __call__(self, v):
+            if not isinstance(v, str):
+                raise TypeError('Expected a naive iso8601 datetime string but found %r' % (v,))
+            return datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S')
+
     conf = json.loads(conf)
 
     return 
