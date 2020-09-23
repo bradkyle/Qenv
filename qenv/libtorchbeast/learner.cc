37475451f0351fe56eeed54c59d08408a22a27d8
Brad Duncan <bradkyleduncan@gmail.com>
2020-09-23 06:28:21 +0200

    Creating agent logic

--- qenv/libtorchbeast/learner.cc
+++ qenv/libtorchbeast/learner.cc
@@ -1,8 +1,32 @@
 
+// import torch
+// import libtorchbeast
+// from torch import nn
+// from torch.nn import functional as F
+// from torchbeast.core import file_writer
+// from torchbeast.core import vtrace
+
 
 // Utilities
 // ------------------------------------------------------------------->
 
+// def compute_baseline_loss(advantages):
+    // return 0.5 * torch.sum(advantages ** 2)
+
+// def compute_entropy_loss(logits):
+//     """Return the entropy loss, i.e., the negative entropy of the policy."""
+//     policy = F.softmax(logits, dim=-1)
+//     log_policy = F.log_softmax(logits, dim=-1)
+//     return torch.sum(policy * log_policy)
+
+// def compute_policy_gradient_loss(logits, actions, advantages):
+//     cross_entropy = F.nll_loss(
+//         F.log_softmax(torch.flatten(logits, 0, 1), dim=-1),
+//         target=torch.flatten(actions, 0, 1),
+//         reduction="none",
+//     )
+//     cross_entropy = cross_entropy.view_as(advantages)
+//     return torch.sum(cross_entropy * advantages.detach())
 
 // Net
 // ------------------------------------------------------------------->
@@ -10,7 +34,6 @@
 
 // Inference
 // ------------------------------------------------------------------->
-
 
 
 // Learn
