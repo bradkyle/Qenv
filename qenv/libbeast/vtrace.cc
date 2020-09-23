3bc1a9cd5dab09b5a17b8f8bbd1ebe5b189743e8
Brad Duncan <bradkyleduncan@gmail.com>
2020-09-23 09:09:20 +0200

    Creating agent logic in c++

--- qenv/libbeast/vtrace.cc
+++ qenv/libbeast/vtrace.cc
@@ -4,14 +4,35 @@
 #include <torch/script.h>
 #include <torch/torch.h>
 
-action_log_probs()  {
-
+torch::Tensor action_log_probs()  {
+    return torch::nll_loss(
+        torch::log_softmax(policy_logits.flatten(0,-2), -1), 
+        actions.flatten(),
+        // TODO reduction
+    ).negative();
 };
+
+from_importance_weights(){
+    torch::NoGradGuard no_grad;
+    torch::Tensor rhos = log_rhos.exp(); // TODO check
+
+    torch::Tensor clipped_rhos;
+    if (clip_rho_threshold) {
+        clipped_rhos = torch::clamp(rhos; );
+    } else {
+        clipped_rhos = rhos; 
+    };
+
+    cs = torch::clamp(rhos; );
+
+    // Append bootstrapped value to get  [v1, ..., v_t+1]
+    torch::Tensor values_t_plus_1 = torch.cat();
+    torch::Tensor deltas = clipped_rhos * (rewards + discounts * values_t_plus_1 - values);
+
+    torch::Tensor acc = torch::zeros_like(); 
+
+}
 
 from_logits(){
 
 };
-
-from_importance_weights(){
-    
-}
