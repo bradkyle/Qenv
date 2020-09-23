eaefdcc49b095ae55c0a3a5faec1624719b886ee
Brad Duncan <bradkyleduncan@gmail.com>
2020-09-23 09:36:59 +0200

    Creating agent logic in c++

--- qenv/libbeast/learn.cc
+++ qenv/libbeast/learn.cc
@@ -4,6 +4,28 @@
 #include <torch/script.h>
 #include <torch/torch.h>
 
-torch::Tensor compute_baseline_loss(torch::Tensor advantages){
+torch::Tensor   compute_baseline_loss(torch::Tensor advantages){
     return 0.5 * torch::sum(advantages.pow(2));
+};
+
+torch::Tensor   compute_entropy_loss(torch::Tensor logits){
+    torch::Tensor policy = torch::softmax(logits, -1);
+    torch::Tensor log_policy = torch::log_softmax(logits, -1);
+    return torch::sum(policy*log_policy);
+};
+
+torch::Tensor   compute_policy_gradient_loss(
+        torch::Tensor logits, 
+        torch::Tensor actions, 
+        torch::Tensor advantages):{
+            torch::Tensor cross_entropy = torch::nll_loss( // TODO check
+                torch::log_softmax(),
+                actions.flatten(),
+                "None" // TODO check
+            );
+
+            return torch::sum(
+                cross_entropy.view_as(advantages) * 
+                advantages.detach()
+            );
 };
