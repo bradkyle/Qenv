1ce39644b2525d858849a55d0c6092b1f02fe500
Brad Duncan <bradkyleduncan@gmail.com>
2020-09-23 07:02:04 +0200

    Creating agent logic

--- qenv/libtorchbeast/learner.cc
+++ qenv/libtorchbeast/learner.cc
@@ -35,6 +35,140 @@
 // Net
 // ------------------------------------------------------------------->
 
+// class Net(nn.Module):
+//     def __init__(self, num_actions, use_lstm=False):
+//         super(Net, self).__init__()
+//         self.num_actions = num_actions
+//         self.use_lstm = use_lstm
+
+//         self.feat_convs = []
+//         self.resnet1 = []
+//         self.resnet2 = []
+
+//         self.convs = []
+
+//         input_channels = 4
+//         for num_ch in [16, 32, 32]:
+//             feats_convs = []
+//             feats_convs.append(
+//                 nn.Conv2d(
+//                     in_channels=input_channels,
+//                     out_channels=num_ch,
+//                     kernel_size=3,
+//                     stride=1,
+//                     padding=1,
+//                 )
+//             )
+//             feats_convs.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+//             self.feat_convs.append(nn.Sequential(*feats_convs))
+
+//             input_channels = num_ch
+
+//             for i in range(2):
+//                 resnet_block = []
+//                 resnet_block.append(nn.ReLU())
+//                 resnet_block.append(
+//                     nn.Conv2d(
+//                         in_channels=input_channels,
+//                         out_channels=num_ch,
+//                         kernel_size=3,
+//                         stride=1,
+//                         padding=1,
+//                     )
+//                 )
+//                 resnet_block.append(nn.ReLU())
+//                 resnet_block.append(
+//                     nn.Conv2d(
+//                         in_channels=input_channels,
+//                         out_channels=num_ch,
+//                         kernel_size=3,
+//                         stride=1,
+//                         padding=1,
+//                     )
+//                 )
+//                 if i == 0:
+//                     self.resnet1.append(nn.Sequential(*resnet_block))
+//                 else:
+//                     self.resnet2.append(nn.Sequential(*resnet_block))
+
+//         self.feat_convs = nn.ModuleList(self.feat_convs)
+//         self.resnet1 = nn.ModuleList(self.resnet1)
+//         self.resnet2 = nn.ModuleList(self.resnet2)
+
+//         self.fc = nn.Linear(3872, 256)
+
+//         # FC output size + last reward.
+//         core_output_size = self.fc.out_features + 1
+
+//         if use_lstm:
+//             self.core = nn.LSTM(core_output_size, 256, num_layers=1)
+//             core_output_size = 256
+
+//         self.policy = nn.Linear(core_output_size, self.num_actions)
+//         self.baseline = nn.Linear(core_output_size, 1)
+
+//     def initial_state(self, batch_size=1):
+//         if not self.use_lstm:
+//             return tuple()
+//         return tuple(
+//             torch.zeros(self.core.num_layers, batch_size, self.core.hidden_size)
+//             for _ in range(2)
+//         )
+
+//     def forward(self, inputs, core_state):
+//         x = inputs["frame"]
+//         T, B, *_ = x.shape
+//         x = torch.flatten(x, 0, 1)  # Merge time and batch.
+//         x = x.float() / 255.0
+
+//         res_input = None
+//         for i, fconv in enumerate(self.feat_convs):
+//             x = fconv(x)
+//             res_input = x
+//             x = self.resnet1[i](x)
+//             x += res_input
+//             res_input = x
+//             x = self.resnet2[i](x)
+//             x += res_input
+
+//         x = F.relu(x)
+//         x = x.view(T * B, -1)
+//         x = F.relu(self.fc(x))
+
+//         clipped_reward = torch.clamp(inputs["reward"], -1, 1).view(T * B, 1)
+//         core_input = torch.cat([x, clipped_reward], dim=-1)
+
+//         if self.use_lstm:
+//             core_input = core_input.view(T, B, -1)
+//             core_output_list = []
+//             notdone = (~inputs["done"]).float()
+//             for input, nd in zip(core_input.unbind(), notdone.unbind()):
+//                 # Reset core state to zero whenever an episode ended.
+//                 # Make `done` broadcastable with (num_layers, B, hidden_size)
+//                 # states:
+//                 nd = nd.view(1, -1, 1)
+//                 core_state = nest.map(nd.mul, core_state)
+//                 output, core_state = self.core(input.unsqueeze(0), core_state)
+//                 core_output_list.append(output)
+//             core_output = torch.flatten(torch.cat(core_output_list), 0, 1)
+//         else:
+//             core_output = core_input
+
+//         policy_logits = self.policy(core_output)
+//         baseline = self.baseline(core_output)
+
+//         if self.training:
+//             action = torch.multinomial(F.softmax(policy_logits, dim=1), num_samples=1)
+//         else:
+//             # Don't sample when testing.
+//             action = torch.argmax(policy_logits, dim=1)
+
+//         policy_logits = policy_logits.view(T, B, self.num_actions)
+//         baseline = baseline.view(T, B)
+//         action = action.view(T, B)
+
+//         return (action, policy_logits, baseline), core_state
+
 
 // Inference
 // ------------------------------------------------------------------->
@@ -162,6 +296,10 @@
 //         lock.release()
 
 
+// Train
+// ------------------------------------------------------------------->
+
+
 // def train(flags):
 //     if flags.xpid is None:
 //         flags.xpid = "torchbeast-%s" % time.strftime("%Y%m%d-%H%M%S")
@@ -206,6 +344,8 @@
 //         check_outputs=True,
 //     )
 
+
+// 
 //     addresses = []
 //     connections_per_server = 1
 //     pipe_id = 0
@@ -242,7 +382,7 @@
 
 //     actorpool_thread = threading.Thread(target=run, name="actorpool-thread")
 
-//     optimizer = torch.optim.RMSprop(
+//     optimizer = torch.optim.RMSprop( //https://pytorch.org/cppdocs/api/classtorch_1_1optim_1_1_r_m_sprop.html#class-documentation
 //         model.parameters(),
 //         lr=flags.learning_rate,
 //         momentum=flags.momentum,
@@ -257,7 +397,7 @@
 //             / flags.total_steps
 //         )
 
-//     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
+//     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda) // C++ doesn't have try find!
 
 //     stats = {}
 
@@ -276,7 +416,7 @@
 //     actor_model.load_state_dict(model.state_dict())
 
 //     learner_threads = [
-//         threading.Thread(
+//         threading.Thread( // TODO find analog for this in c++
 //             target=learn,
 //             name="learner-thread-%i" % i,
 //             args=(
@@ -294,7 +434,7 @@
 //     ]
     
 //     inference_threads = [
-//         threading.Thread(
+//         threading.Thread( // TODO find analog fot this in c++
 //             target=inference,
 //             name="inference-thread-%i" % i,
 //             args=(flags, inference_batcher, actor_model),
@@ -310,7 +450,7 @@
 //         if flags.disable_checkpoint:
 //             return
 //         logging.info("Saving checkpoint to %s", checkpointpath)
-//         torch.save(
+//         torch.save( // https://pytorch.org/cppdocs/api/function_namespacetorch_1a99dc9f736064b2179cc58e6436f7a021.html
 //             {
 //                 "model_state_dict": model.state_dict(),
 //                 "optimizer_state_dict": optimizer.state_dict(),
@@ -351,7 +491,7 @@
 //                     f"{key} = {format_value(value)}" for key, value in stats.items()
 //                 ),
 //             )
-//     except KeyboardInterrupt:
+//     except KeyboardInterrupt: // TODO replacement
 //         pass  # Close properly.
 //     else:
 //         logging.info("Learning finished after %i steps.", stats["step"])
