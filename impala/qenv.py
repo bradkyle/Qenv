7e51b089aae67d71c942b4e8d23c2fbc6819dd4d
bradkyle <bradkyleduncan@gmail.com>
2020-12-05 07:45:22 +0000

    implementing fill tests

--- impala/qenv.py
+++ impala/qenv.py
@@ -3,13 +3,12 @@ import numpy as np
 from qpython import qconnection
 
 # TODO 
-class Qenv(gym.Env):
-    def __init__(self, host, port):
+class MultiQenv(gym.Env):
+    def __init__(self, num_actors, host, port):
         self.shape = (256,)
         self.port = port
         self.host = host
-        # self.obs_space = self._observation_space()
-        # self.act_space = self._action_space()
+        self.num_actors = num_actors
 
     def _req(self, qry):
         with qconnection.QConnection(host=self.host, port=self.port) as q:
@@ -31,6 +30,3 @@ class Qenv(gym.Env):
         data = self._req(".env.Reset[]")
         return np.array(data.items()[0][1].tolist())
 
-class QMultiEnv(gym.Env):
-    def __init__(self, host, port):
-        pass
