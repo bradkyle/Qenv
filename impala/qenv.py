30fb14fe3473c7fe5730cacf78b4939b52bff792
bradkyle <bradkyleduncan@gmail.com>
2020-12-05 07:08:24 +0000

    implementing fill tests

--- impala/qenv.py
+++ impala/qenv.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np
 from qpython import qconnection
 
+# TODO 
 class Qenv(gym.Env):
     def __init__(self, host, port):
         self.shape = (256,)
@@ -16,25 +17,17 @@ class Qenv(gym.Env):
 
     @property
     def observation_space(self):
-        self.obs_space
+        return gym.spaces.Box(0, 1, shape=(136,))
 
     @property
     def action_space(self):
-        self.act_space
-
-    def _observation_space(self):
-        data = self._req(".env.FeatureSpace")
-        return gym.spaces.Box(0, 1, shape=(data,))
-
-    def _action_space(self):
-        data = self._req(".env.ActionSpace")
-        return gym.spaces.Discrete(data)
+        return gym.spaces.Discrete(22)
 
     def step(self, actions):
-        data = self._req(".env.Step["+str(actions)+"]")
-        return (np.array(data[0]), data[1], data[2], {})
+        data = self._req(".env.Step[enlist(0;"+str(actions)+")]")
+        return (data[0].items()[0][1], data[1].items()[0][0][0], data[2][0], {})
 
     def reset(self):
         data = self._req(".env.Reset[]")
-        return np.array(data)
+        return data.items()[0][1]
 
